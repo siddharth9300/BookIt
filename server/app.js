18395bdb793bf1622899a805dfad58ded8914500
siddharth9300 <jkrish930@gmail.com>
2023-05-14 14:30:02 +0530

    cookie changes

--- server/app.js
+++ server/app.js
@@ -6,18 +6,22 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser");
 const app = express();
 app.use(cookieParser());
-app.use(cors({credentials: true,
+
+app.use(cors({
+  credentials: true,
   origin: true})); 
   
-  app.set("trust proxy",1); 
+app.set("trust proxy",1); 
+
+
 app.use(express.json());
 
 dotenv.config({path:"./.env"})
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Credentials', true);
+//   next();
+// });
 const connectDB = require("./DB/conn")
 require("./model/userSchema")
 require("./model/hallSchema")
