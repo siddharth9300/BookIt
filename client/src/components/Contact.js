2d3c5fb3a9dfe39ba938e9af897266ebd3315340
Siddharth <jkrish930@gmail.com>
2023-05-18 16:40:08 +0530

    added contact details

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -4,14 +4,14 @@ const Contact = () => {
 
 
   return (
-    <div className="flex min-h-screen w-full items-center justify-center m-4">
-    <div className="w-full rounded-xl p-12 shadow-2xl shadow-blue-200 md:w-8/12 lg:w-6/12 bg-white">
+    <div className="flex min-h-screen w-full flex-wrap items-center justify-center m-4">
+    <div className="w-full rounded-xl p-12 shadow-2xl shadow-blue-200 mx-auto md:w-8/12 lg:w-5/12 bg-white">
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
       
         <div className="col-span-1 lg:col-span-9">
           <div className="text-center lg:text-left">
-            <h2 className="text-2xl font-bold text-zinc-700">contact person name</h2>
-            <p className="mt-2 text-l font-semibold text-zinc-700">Faculty</p>
+            <h2 className="text-2xl font-bold text-zinc-700">Prof. Nitin Kulkarni</h2>
+            <p className="mt-2 text-l font-semibold text-zinc-700">Faculty Coordinator</p>
 
           </div>
 
@@ -22,7 +22,7 @@ const Contact = () => {
             </div>
 
             <div>
-              <p className="text-m font-semibold text-zinc-700">contact person email</p>
+              <p className="text-m font-semibold text-zinc-700">nitinkulkarni@acropolis.in</p>
             </div>
 
           </div>
@@ -33,7 +33,7 @@ const Contact = () => {
             </div>
 
             <div>
-              <p className="text-m font-semibold text-zinc-700">contact person phone</p>
+              <p className="text-m font-semibold text-zinc-700">9827253854</p>
             </div>
           </div>
 
@@ -43,44 +43,135 @@ const Contact = () => {
 
 
 
+    </div>
+
+
+
+
+
+    <div className="w-full rounded-xl p-12 shadow-2xl mx-auto shadow-blue-200 md:w-8/12 lg:w-5/12 bg-white">
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
       
-      <div className="col-span-1 lg:col-span-9">
-        <div className="text-center lg:text-left">
-          <h2 className="text-2xl font-bold text-zinc-700">contact person name</h2>
-          <p className="mt-2 text-l font-semibold text-zinc-700">Faculty</p>
+        <div className="col-span-1 lg:col-span-9">
+          <div className="text-center lg:text-left">
+            <h2 className="text-2xl font-bold text-zinc-700">Siddharth Jain</h2>
+            <p className="mt-2 text-l font-semibold text-zinc-700">Studenet Coordinator</p>
+
+          </div>
+
+    
+          <div className="mt-6 grid grid-cols-3 gap-8 text-center items-center lg:text-left">
+            <div>
+              <p className="font-bold text-zinc-700">Email</p>
+            </div>
+
+            <div>
+              <p className="text-m font-semibold text-zinc-700">siddharthjain.ca21@acropolis.in</p>
+            </div>
+
+          </div>
+
+          <div className="mt-6 grid grid-cols-3 gap-8  text-center items-center lg:text-left">
+            <div>
+              <p className="font-bold text-zinc-700">Phone</p>
+            </div>
+
+            <div>
+              <p className="text-m font-semibold text-zinc-700">7898210160</p>
+            </div>
+          </div>
 
         </div>
-
-  
-        <div className="mt-6 grid grid-cols-3 gap-8 text-center items-center lg:text-left">
-          <div>
-            <p className="font-bold text-zinc-700">Email</p>
-          </div>
-
-          <div>
-            <p className="text-m font-semibold text-zinc-700">contact person email</p>
-          </div>
-
-        </div>
-
-        <div className="mt-6 grid grid-cols-3 gap-8  text-center items-center lg:text-left">
-          <div>
-            <p className="font-bold text-zinc-700">Phone</p>
-          </div>
-
-          <div>
-            <p className="text-m font-semibold text-zinc-700">contact person phone</p>
-          </div>
-        </div>
-
       </div>
-    </div>
 
 
 
 
     </div>
+
+    
+    <div className="w-full rounded-xl p-12 shadow-2xl mx-auto shadow-blue-200 md:w-8/12 lg:w-5/12 bg-white">
+      <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
+      
+        <div className="col-span-1 lg:col-span-9">
+          <div className="text-center lg:text-left">
+            <h2 className="text-2xl font-bold text-zinc-700">Ashutosh Dubey</h2>
+            <p className="mt-2 text-l font-semibold text-zinc-700">Studenet Coordinator</p>
+
+          </div>
+
+    
+          <div className="mt-6 grid grid-cols-3 gap-8 text-center items-center lg:text-left">
+            <div>
+              <p className="font-bold text-zinc-700">Email</p>
+            </div>
+
+            <div>
+              <p className="text-m font-semibold text-zinc-700">ashutoshdubey.ca21@acropolis.in</p>
+            </div>
+
+          </div>
+
+          <div className="mt-6 grid grid-cols-3 gap-8  text-center items-center lg:text-left">
+            <div>
+              <p className="font-bold text-zinc-700">Phone</p>
+            </div>
+
+            <div>
+              <p className="text-m font-semibold text-zinc-700">8269690492</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+
+
+    </div>
+
+
+
+ 
+    <div className="w-full rounded-xl p-12 shadow-2xl mx-auto shadow-blue-200 md:w-8/12 lg:w-5/12 bg-white">
+      <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
+      
+        <div className="col-span-1 lg:col-span-9">
+          <div className="text-center lg:text-left">
+            <h2 className="text-2xl font-bold text-zinc-700">Abhishek Kumar</h2>
+            <p className="mt-2 text-l font-semibold text-zinc-700">Studenet Coordinator</p>
+
+          </div>
+
+    
+          <div className="mt-6 grid grid-cols-3 gap-8 text-center items-center lg:text-left">
+            <div>
+              <p className="font-bold text-zinc-700">Email</p>
+            </div>
+
+            <div>
+              <p className="text-m font-semibold text-zinc-700">abhishekkumar.ca21@acropolis.in</p>
+            </div>
+
+          </div>
+
+          <div className="mt-6 grid grid-cols-3 gap-8  text-center items-center lg:text-left">
+            <div>
+              <p className="font-bold text-zinc-700">Phone</p>
+            </div>
+
+            <div>
+              <p className="text-m font-semibold text-zinc-700">9955357820</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </div>
+
+
+
   </div>
   );
 };
