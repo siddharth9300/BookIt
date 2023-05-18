30f3cb5ee87be83866262745d5021e45fe7e33b4
Siddharth <83226178+siddharth9300@users.noreply.github.com>
2023-05-18 16:18:19 +0530

    chagnge contact

---
+++ client/src/components/Contact copy.js
@@ -0,0 +1,198 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const Contact = () => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const userContact = async () => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/getdata`, {
+        withCredentials: true, // include credentials in the request
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = response.data;
+      console.log(data);
+      setUserData({
+        ...userData,
+        name: data.name,
+        email: data.email,
+        phone: data.phone,
+      });
+
+      if (response.status !== 200) {
+        throw new Error(response.error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    userContact();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // handle change here
+
+  const handleInputs = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  console.log(userData);
+
+  // send to backend
+
+  const contactForm = async (e) => {
+    e.preventDefault();
+    const { name, email, phone, message } = userData;
+
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/contact`,
+        {
+          name,
+          email,
+          phone,
+          message,
+        },
+        {
+          withCredentials: true, // To include credentials in the request
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = response.data;
+
+      if (!data) {
+        console.log("Message not send");
+      } else {
+        alert("Message send");
+        setUserData({ ...userData, message: "" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <div className="max-w-screen-md mx-auto p-5 my-10 bg-white shadow-2xl shadow-blue-200">
+        <div className="text-center mb-16">
+          <p className="mt-4 text-sm leading-7 text-gray-500 font-regular uppercase">
+            Contact
+          </p>
+          <h3 className="text-3xl sm:text-4xl leading-normal font-extrabold tracking-tight text-gray-900">
+            Get In <span className="text-indigo-600">Touch</span>
+          </h3>
+        </div>
+
+        <form method="POST" className="w-full">
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2 "
+                for="grid-first-name"
+              >
+                Name
+              </label>
+              <input
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="text"
+                value={userData.name}
+                name="name"
+                onChange={handleInputs}
+                placeholder="Name"
+              />
+              {/* <p className="text-red-500 text-xs italic">Please fill out this field.</p> */}
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-last-name"
+              >
+                Phone
+              </label>
+              <input
+                value={userData.phone}
+                name="phone"
+                onChange={handleInputs}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-last-name"
+                type="number"
+                placeholder="Phone"
+              />
+            </div>
+          </div>
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-password"
+              >
+                Email Address
+              </label>
+              <input
+                value={userData.email}
+                name="email"
+                onChange={handleInputs}
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                id="grid-email"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full px-3">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                for="grid-password"
+              >
+                Your Message
+              </label>
+              <textarea
+                value={userData.message}
+                name="message"
+                onChange={handleInputs}
+                rows="10"
+                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              ></textarea>
+            </div>
+            <div className="flex justify-between w-full px-3">
+              {/* <div className="md:flex md:items-center">
+        <label className="block text-gray-500 font-bold">
+          <input className="mr-2 leading-tight" type="checkbox"/>
+          <span className="text-sm">
+            Send me your newsletter!
+          </span>
+        </label>
+      </div> */}
+              <button
+                onClick={contactForm}
+                className="shadow bg-indigo-600 hover:bg-indigo-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-6 rounded"
+                type="submit"
+              >
+                Send Message
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
