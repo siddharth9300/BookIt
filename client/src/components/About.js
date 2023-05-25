de211caa244ea541f6311d4f164a3e747d8f0f64
Siddharth <jkrish930@gmail.com>
2023-05-25 19:38:28 +0530

    user verificaion mail is sent to admin

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -7,14 +7,9 @@ const About = () => {
   const navigate = useNavigate();
   const [userData, setUserData] = useState({});
   const [isLoading, setIsLoading] = useState(true);
-
-
-
   const email = userData.email
   const emailVerified = userData.emailVerified
-
   const VerifyButton = () => {
-
     if (emailVerified) {
       return (
         <>
@@ -26,9 +21,7 @@ const About = () => {
         </>
       )
     } else {
-
       return (
-
         <>
           <button
             type="submit"
@@ -41,94 +34,61 @@ const About = () => {
       )
     }
   }
-
-
-
-
   const sendEmailVerificationLink = async (e) => {
     setIsLoading(true)
     e.preventDefault();
-
-
     try {
       const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/emailVerificationLink`, {
         email
       }, {
-        withCredentials: true, // include credentials in the request
+        withCredentials: true, 
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json"
         },
       });
-
       const data = response.data
-
       if (data) {
         setIsLoading(false)
-
-        // setAuthStatus("Please Check yout Email")    
         toast.success("Email Sent Successfull")
       }
-      // setAuthStatus("")
-
     } catch (error) {
       if (error.response.status === 400 && error.response) {
         const data = error.response.data;
-        // setAuthStatus(data.error)
         console.log(data.error)
-        // window.alert(data.error);
       } else {
-        // setAuthStatus("Something Went Worng")
         console.log(error.response.data)
-
       }
     }
   };
-
-
-
   const callAboutPage = async () => {
     try {
       const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/about`, {
-        withCredentials: true, // include credentials in the request
+        withCredentials: true, 
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json"
         }
       });
-
       const data = response.data;
       console.log(data);
       setUserData(data);
       setIsLoading(false);
-
       if (response.status !== 200) {
         throw new Error(response.error);
       }
     } catch (error) {
-      // console.log(error);
       if (error.response.status === 401) {
         toast.warn("Unauthrized Access! Please Login!", {
           toastId: 'Unauthrized'
       })
         navigate("/login");
       }
-      // navigate("/login");
     }
   };
-
-
-
   useEffect(() => {
-
     callAboutPage()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-
-
-
-
-
   return (
     <>
       {isLoading ? (
@@ -137,160 +97,46 @@ const About = () => {
         <div className="flex min-h-screen w-full items-center justify-center m-4">
           <div className="w-full rounded-xl p-12 shadow-2xl shadow-blue-200 md:w-8/12 lg:w-6/12 bg-white">
             <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
-              {/* <div className="grid-cols-1 lg:col-span-3">
-              <div className="mx-auto flex h-[90px] w-[90px] items-center justify-center rounded-full bg-blue-100 p-4">
-                <svg
-                  id="logo-39"
-                  width="50"
-                  height="40"
-                  viewBox="0 0 50 40"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M25.0001 0L50 15.0098V24.9863L25.0001 40L0 24.9863V15.0099L25.0001 0Z"
-                    fill="#A5B4FC"
-                    className="ccompli2"
-                  ></path>
-                  <path
-                    fill-rule="evenodd"
-                    clip-rule="evenodd"
-                    d="M0 15.0098L25 0L50 15.0098V24.9863L25 40L0 24.9863V15.0098ZM25 33.631L44.6967 21.8022V18.1951L44.6957 18.1945L25 30.0197L5.30426 18.1945L5.3033 18.1951V21.8022L25 33.631ZM25 24.5046L40.1018 15.4376L36.4229 13.2298L25 20.0881L13.5771 13.2298L9.89822 15.4376L25 24.5046ZM25 14.573L31.829 10.4729L25 6.37467L18.171 10.4729L25 14.573Z"
-                    fill="#4F46E5"
-                    className="ccustom"
-                  ></path>
-                  <path
-                    d="M25.0001 0L0 15.0099V24.9863L25 40L25.0001 0Z"
-                    fill="#A5B4FC"
-                    className="ccompli2"
-                    fill-opacity="0.3"
-                  ></path>
-                </svg>
-              </div>
-            </div> */}
-
               <div className="col-span-1 lg:col-span-9">
                 <div className="text-center lg:text-left">
                   <h2 className="text-2xl font-bold text-zinc-700">{userData.name}</h2>
                   <p className="mt-2 text-l font-semibold text-zinc-700">{userData.userType}</p>
-                  {/* <p className="mt-4 text-zinc-500">I am a Front End Developer and UI/UX Designer</p> */}
                 </div>
-
                 <div className="mt-6 grid grid-cols-3 gap-8 text-center items-center lg:text-left">
                   <div>
                     <p className="font-bold text-zinc-700">User Id</p>
                   </div>
-
                   <div>
                     <p className="text-m font-semibold text-zinc-700">{userData._id}</p>
                   </div>
                 </div>
-
-                {/* <div className="mt-6 grid grid-cols-2 gap-6 text-center lg:text-left">
-                <div>
-                  <p className="font-bold text-zinc-700">Name</p>
-                </div>
-
-                <div>
-                  <p className="text-m font-semibold text-zinc-700">Name</p>
-                </div>  
-              </div> */}
-
-
-
                 <div className="mt-6 grid grid-cols-3 gap-8 text-center items-center lg:text-left">
                   <div>
                     <p className="font-bold text-zinc-700">Email</p>
                   </div>
-
                   <div>
                     <p className="text-m font-semibold text-zinc-700">{userData.email}</p>
                   </div>
-
                   <div>
-
                     <VerifyButton />
-                    {/* {emailVerified? 
-                  <button
-                  type="submit"
-                  
-                  onClick={sendEmailVerificationLink}
-                  className="text-white bg-indigo-600 shadow focus:shadow-outline ml-6  focus:outline-none border-0 py-2 px-5 font-bold  hover:bg-indigo-800 rounded text-sm"
-                  >
-                  Verify Email
-                   </button>
-
-                    :
-                    <button
-                    type="submit"
-                    
-                    onClick={sendEmailVerificationLink}
-                    className="text-white bg-green-600 shadow focus:shadow-outline ml-6  focus:outline-none border-0 py-2 px-5 font-bold  hover:bg-indigo-800 rounded text-sm"
-                    >
-                    Verifyed
-                     </button> 
-                     } */}
                   </div>
                 </div>
-
-                {/* 
-              <div className="mx-auto w-fit ">
-              <div className="mx-auto">
-                <button
-                  type="submit"
-                  
-                  onClick={sendEmailVerificationLink}
-                  className="text-white bg-indigo-600 shadow focus:shadow-outline focus:outline-none border-0 py-2 px-5 font-bold  hover:bg-indigo-800 rounded text-sm"
-                >
-                  Verify Email
-                </button>
-              </div>
-            </div> */}
-
-
-
                 <div className="mt-6 grid grid-cols-3 gap-8  text-center items-center lg:text-left">
                   <div>
                     <p className="font-bold text-zinc-700">Department</p>
                   </div>
-
                   <div>
                     <p className="text-m font-semibold text-zinc-700">{userData.department}</p>
                   </div>
                 </div>
-
-
                 <div className="mt-6 grid grid-cols-3 gap-8  text-center items-center lg:text-left">
                   <div>
                     <p className="font-bold text-zinc-700">Phone</p>
                   </div>
-
                   <div>
                     <p className="text-m font-semibold text-zinc-700">{userData.phone}</p>
                   </div>
                 </div>
-
-
-
-
-
-
-
-
-
-
-                {/* 
-              <div className="mt-6 grid grid-cols-2 gap-4">
-                <button className="w-full rounded-xl border-2 border-blue-500 bg-white px-3 py-2 font-semibold text-blue-500 hover:bg-blue-500 hover:text-white">
-                  Follow
-                </button>
-
-                <button className="w-full rounded-xl border-2 border-blue-500 bg-white px-3 py-2 font-semibold text-blue-500 hover:bg-blue-500 hover:text-white">
-                  View Profile
-                </button>
-              </div> */}
-
-
               </div>
             </div>
           </div>
@@ -299,5 +145,4 @@ const About = () => {
     </>
   );
 };
-
 export default About;
--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -49,7 +49,7 @@ const register = async (req, res,next) => {
     }
     
     const acropolisEmailRegex = /@acropolis\.in$/;
-    const acropolisEduEmailRegex = /@acropolis\.in$/;
+    const acropolisEduEmailRegex = /@acropolis\.edu\.in$/;
 
     if (!acropolisEmailRegex.test(email) || !acropolisEduEmailRegex.test(email) ) {
       return res.status(422).json({ error: "Please enter a email address associated with Acropolis Institute" });
@@ -204,101 +204,230 @@ const register = async (req, res,next) => {
     `;
   };
 
-  const verifyEmailTemplate = (resetLink,userName) => {
+//   const verifyEmailTemplate = (resetLink,userName) => {
+//     return `
+    
+// <head>
+// <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+// <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+// <style>
+//   a,
+//   a:link,
+//   a:visited {
+//     text-decoration: none;
+//     color: #00788a;
+//   }
+
+//   a:hover {
+//     text-decoration: underline;
+//   }
+
+//   h2,
+//   h2 a,
+//   h2 a:visited,
+//   h3,
+//   h3 a,
+//   h3 a:visited,
+//   h4,
+//   h5,
+//   h6,
+//   .t_cht {
+//     color: #000 !important;
+//   }
+
+//   .ExternalClass p,
+//   .ExternalClass span,
+//   .ExternalClass font,
+//   .ExternalClass td {
+//     line-height: 100%;
+//   }
+
+//   .ExternalClass {
+//     width: 100%;
+//   }
+// </style>
+// </head>
+
+// <body style="font-size: 1.25rem;font-family: 'Roboto', sans-serif;padding-left:20px;padding-right:20px;padding-top:20px;padding-bottom:20px; background-color: #FAFAFA; width: 75%; max-width: 1280px; min-width: 600px; margin-right: auto; margin-left: auto">
+// <table cellpadding="12" cellspacing="0" width="100%" bgcolor="#FAFAFA" style="border-collapse: collapse;margin: auto">
+//   <thead>
+//     <tr>
+//       <td style="padding-left: 0; padding-right: 0">
+//         <img src="https://gcdnb.pbrd.co/images/zVpWVNL2VI1Y.png?o=1" style="width:80%; max-width:750px" />
+//       </td>
+//     </tr>
+//     <tr>
+//       <td style="text-align:center; padding-bottom: 20px">
+//         <img src="https://gcdnb.pbrd.co/images/VXm0SxliFZYt.png?o=1" style="max-width: 250px; width: 40%;" />
+//       </td>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     <tr>
+//       <td style="padding: 50px; background-color: #fff; max-width: 660px">
+//         <table width="100%" style="">
+//           <tr>
+//             <td style="text-align:center">
+//               <h1 style="font-size: 30px; color: #202225; margin-top: 0;">Hello ${userName}</h1>
+//               <p style="font-size: 18px; margin-bottom: 30px; color: #202225; max-width: 60ch; margin-left: auto; margin-right: auto">A request has been received to verify your email address for your account.<br>Below link will expire in 5 minutes.</p>
+//               <a href="${resetLink}"  style="background-color: #4f46e5; color: #fff; padding: 8px 24px; border-radius: 8px; border-style: solid; border-color: #4f46e5; font-size: 14px; text-decoration: none; cursor: pointer">Verify Email </a>
+//             </td>
+//           </tr>
+//         </table>
+//       </td>
+//     </tr>
+//   </tbody>
+//   <tfoot>
+//     <tr>
+//       <td style="text-align: center; padding-top: 30px">
+//         <table>
+//           <tr>
+//             <td><img src="https://uploads-ssl.webflow.com/5e96c040bda7162df0a5646d/5f91d2a4d80f5ebbf2ec0119_image-hand%20with%20wrench%402x.png" style="width: 100px;" /></td>
+//             <td>
+//             <td style="text-align: left;color:#B6B6B6; font-size: 18px; padding-left: 12px">If you didn’t request this, you can ignore this email or let us know.</td>
+//       </td>
+      
+//     </tr>
+// </table>
+
+// </td>
+// </tr>
+// </tfoot>
+// </table>
+// </body>
+
+
+
+//     `;
+//   };
+
+
+
+
+
+
+
+
+  
+  const verifyEmailTemplate = (resetLink,userFind) => {
     return `
     
-<head>
-<meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
-<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-<style>
-  a,
-  a:link,
-  a:visited {
-    text-decoration: none;
-    color: #00788a;
-  }
 
-  a:hover {
-    text-decoration: underline;
-  }
-
-  h2,
-  h2 a,
-  h2 a:visited,
-  h3,
-  h3 a,
-  h3 a:visited,
-  h4,
-  h5,
-  h6,
-  .t_cht {
-    color: #000 !important;
-  }
-
-  .ExternalClass p,
-  .ExternalClass span,
-  .ExternalClass font,
-  .ExternalClass td {
-    line-height: 100%;
-  }
-
-  .ExternalClass {
-    width: 100%;
-  }
-</style>
-</head>
-
-<body style="font-size: 1.25rem;font-family: 'Roboto', sans-serif;padding-left:20px;padding-right:20px;padding-top:20px;padding-bottom:20px; background-color: #FAFAFA; width: 75%; max-width: 1280px; min-width: 600px; margin-right: auto; margin-left: auto">
-<table cellpadding="12" cellspacing="0" width="100%" bgcolor="#FAFAFA" style="border-collapse: collapse;margin: auto">
-  <thead>
-    <tr>
-      <td style="padding-left: 0; padding-right: 0">
-        <img src="https://gcdnb.pbrd.co/images/zVpWVNL2VI1Y.png?o=1" style="width:80%; max-width:750px" />
-      </td>
-    </tr>
-    <tr>
-      <td style="text-align:center; padding-bottom: 20px">
-        <img src="https://gcdnb.pbrd.co/images/VXm0SxliFZYt.png?o=1" style="max-width: 250px; width: 40%;" />
-      </td>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td style="padding: 50px; background-color: #fff; max-width: 660px">
-        <table width="100%" style="">
-          <tr>
-            <td style="text-align:center">
-              <h1 style="font-size: 30px; color: #202225; margin-top: 0;">Hello ${userName}</h1>
-              <p style="font-size: 18px; margin-bottom: 30px; color: #202225; max-width: 60ch; margin-left: auto; margin-right: auto">A request has been received to verify your email address for your account.<br>Below link will expire in 5 minutes.</p>
-              <a href="${resetLink}"  style="background-color: #4f46e5; color: #fff; padding: 8px 24px; border-radius: 8px; border-style: solid; border-color: #4f46e5; font-size: 14px; text-decoration: none; cursor: pointer">Verify Email </a>
-            </td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-  </tbody>
-  <tfoot>
-    <tr>
-      <td style="text-align: center; padding-top: 30px">
-        <table>
-          <tr>
-            <td><img src="https://uploads-ssl.webflow.com/5e96c040bda7162df0a5646d/5f91d2a4d80f5ebbf2ec0119_image-hand%20with%20wrench%402x.png" style="width: 100px;" /></td>
-            <td>
-            <td style="text-align: left;color:#B6B6B6; font-size: 18px; padding-left: 12px">If you didn’t request this, you can ignore this email or let us know.</td>
-      </td>
-    </tr>
-</table>
-
+    <head>
+    <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <style>
+      a,
+      a:link,
+      a:visited {
+        text-decoration: none;
+        color: #00788a;
+      }
+    
+      a:hover {
+        text-decoration: underline;
+      }
+    
+      h2,
+      h2 a,
+      h2 a:visited,
+      h3,
+      h3 a,
+      h3 a:visited,
+      h4,
+      h5,
+      h6,
+      .t_cht {
+        color: #000 !important;
+      }
+    
+      .ExternalClass p,
+      .ExternalClass span,
+      .ExternalClass font,
+      .ExternalClass td {
+        line-height: 100%;
+      }
+    
+      .ExternalClass {
+        width: 100%;
+      }
+    </style>
+    </head>
+    
+    <body style="font-size: 1.25rem;font-family: 'Roboto', sans-serif;padding-left:20px;padding-right:20px;padding-top:20px;padding-bottom:20px; background-color: #FAFAFA; width: 75%; max-width: 1280px; min-width: 600px; margin-right: auto; margin-left: auto">
+    <table cellpadding="12" cellspacing="0" width="100%" bgcolor="#FAFAFA" style="border-collapse: collapse;margin: auto">
+      <thead>
+        <tr>
+          <td style="padding-left: 0; padding-right: 0">
+            <img src="https://gcdnb.pbrd.co/images/zVpWVNL2VI1Y.png?o=1" style="width:80%; max-width:750px" />
+          </td>
+        </tr>
+        <tr>
+          <td style="text-align:center; padding-bottom: 20px">
+            <img src="https://gcdnb.pbrd.co/images/VXm0SxliFZYt.png?o=1" style="max-width: 250px; width: 40%;" />
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td style="padding: 50px; background-color: #fff; max-width: 660px">
+          <table width="100%" style="">
+            <tr>
+              <td style="text-align:center">
+                <h1 style="font-size: 30px; color: #202225; margin-top: 0;">Hello Admin</h1>
+                <p style="font-size: 18px; margin-bottom: 30px; color: #202225; max-width: 60ch; margin-left: auto; margin-right: auto">A new user has registered on our platform. Please review the user's details provided below and click the button below to verify the user.</p>
+                 <h1 style="font-size: 25px;text-align: left; color: #202225; margin-top: 0;">User Details</h1>
+                <div style="text-align: justify; margin:20px; display: flex;">
+                  
+                  <div style="flex: 1; margin-right: 20px;">
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">Full Name :</h1>
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">Email :</h1>
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">Phone :</h1>
+                         <h1 style="font-size: 20px; color: #202225; margin-top: 0;">Profession :</h1>
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">Institution :</h1>
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">Department :</h1>
+                  </div>
+                  <div style="flex: 1;">
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">${userFind.name}</h1>
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">${userFind.email}</h1>
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">${userFind.phone}</h1>
+                         <h1 style="font-size: 20px; color: #202225; margin-top: 0;">${userFind.userType}</h1>
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">${userFind.institution}</h1>
+                    <h1 style="font-size: 20px; color: #202225; margin-top: 0;">${userFind.department}</h1>
+                  </div>
+                </div>
+                
+                <a href="${resetLink}" style="background-color: #4f46e5; color: #fff; padding: 8px 24px; border-radius: 8px; border-style: solid; border-color: #4f46e5; font-size: 14px; text-decoration: none; cursor: pointer">Verify User</a>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+       <td style="text-align: center; padding-top: 30px">
+         <table>
+           <tr>
+             <td><img src="https://gcdnb.pbrd.co/images/VXm0SxliFZYt.png?o=1" style="width: 150px;" /></td>
+           <td>
+            <td style="text-align: left;color:#B6B6B6; font-size: 18px; padding-left: 12px">Thank you for your attention and cooperation. If you have any questions or need further assistance, please feel free to contact our support team.
 </td>
-</tr>
-</tfoot>
+       </td>
+      
+     </tr>
 </table>
-</body>
-
+    
+    </td>
+    </tr>
+    </tfoot>
+    </table>
+    </body>
 
 
     `;
   };
+
 const passwordLink = async (req, res,next) => {
   // console.log(req.body);
   // res.json({message:"login success"})
@@ -433,6 +562,7 @@ const emailVerificationLink = async (req, res,next) => {
   try {
 
     const { email } = req.body;
+
     if (!email ) {
       return res.status(400).json({ error: "Please Enter yout Email" });
     }
@@ -441,7 +571,7 @@ const emailVerificationLink = async (req, res,next) => {
 
     if (userFind) {
         const token = jwt.sign({_id:userFind._id},process.env.SECRET_KEY,{
-          expiresIn:"300s"
+          expiresIn:"1d"
         })
         
         const setUserToken = await User.findByIdAndUpdate({_id:userFind._id},{verifyToken:token},{new:true})
@@ -450,9 +580,11 @@ const emailVerificationLink = async (req, res,next) => {
         if (setUserToken) {
           const mailOptions = {
             from:process.env.SENDER_EMAIL,
-            to:email,
-            subject:"Book It Email Verification",
-            html:verifyEmailTemplate((`${process.env.CLIENT_URL}/verifyEmail/${userFind.id}/${setUserToken.verifyToken}`),userFind.name)
+            // to:email,
+            //send mail to admin to verify new user
+            to:process.env.ADMIN_EMAIL,
+            subject:"Book It User Verification",
+            html:verifyEmailTemplate((`${process.env.CLIENT_URL}/verifyEmail/${userFind.id}/${setUserToken.verifyToken}`),userFind) 
             // text:`This link is valid for 5 minutes \n ${process.env.CLIENT_URL}/forgotPassword/${userFind.id}/${setUserToken.verifyToken} \n click on above link`
           }
         
