efeb699a8dc837ae59eea7bf4015f5a4c17721f7
Siddharth <jkrish930@gmail.com>
2024-01-03 16:53:34 +0530

    typo fix

--- client/src/components/bookings/Events.js
+++ client/src/components/bookings/Events.js
@@ -214,7 +214,7 @@ const Events = () => {
                       </div>
                       <div className="grid grid-cols-3 max-md:grid-cols-1 gap-4 px-2 w-full">
                         <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4 shadow-3xl shadow-shadow-500 dark:!bg-navy-700 dark:shadow-none">
-                          <p className="text-m font-bold text-gray-600">Event Venu</p>
+                          <p className="text-m font-bold text-gray-600">Event Venue</p>
                           <p className="text-base font-medium text-navy-700   ">
                           {event.bookedHallName}
                           </p>
